70d1e0cfa5bb2dd366365ba2c3d16ea18242d39d
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-02-24 20:36:53 -0300

    fix:changing form submission to search for products according to what is entered in the input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,40 +1,47 @@
 'use client'
 
-import { Search } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { FormEvent } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import { Search } from 'lucide-react'
+
 
 export function InputSearch() {
+  const [inputValue, setInputValue] = useState<string>('')
+
   const router = useRouter()
   const searchParams = useSearchParams()
 
   const query = searchParams.get('q')
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
 
-  function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const formData = new FormData(event.currentTarget)
-
-    const data = Object.fromEntries(formData)
-
-    const query = data.q
-
-    if (!query) return null
-
-    router.push(`/search?q=${query}`)
   }
 
+  const delayedQuery = useDebouncedCallback((query: string) => {
+    router.push(`/search?q=${query}`);
+  }, 200);
+
+
+
+  useEffect(() => {
+    delayedQuery(inputValue);
+  }, [delayedQuery, inputValue])
+
   return (
-    <form
-      onSubmit={handleFormSubmit}
-      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
-    >
-      <Search className="w-5 h-5 text-zinc-500" />
+    <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+    
+       <Search className="w-5 h-5 text-zinc-500" /> 
+
       <input
-        name="q"
-        defaultValue={query ?? ''}
         className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
         placeholder="Search Products"
+        defaultValue={query ?? ''}
+        onChange={handleChange}
+        value={inputValue}
+        name="q"
       />
     </form>
   )
