98e7bd75aee61e64b7234db384351a65422ea007
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-02-24 18:47:35 -0300

    feat:create cart context

--- src/context/cart-context.tsx
+++ src/context/cart-context.tsx
@@ -1,13 +1,15 @@
+'use client'
+
 import { ReactNode, createContext, useContext, useState } from 'react'
 
 interface CartItem {
-  productId: string
+  productId: number
   quantity: number
 }
 
 interface CartContextProps {
   items: CartItem[]
-  addToCart: (productId: string) => void
+  addToCart: (productId: number) => void
 }
 
 const CartContext = createContext({} as CartContextProps)
@@ -15,7 +17,7 @@ const CartContext = createContext({} as CartContextProps)
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [cartTtem, setCartItem] = useState<CartItem[]>([])
 
-  function addToCart(productId: string) {
+  function addToCart(productId: number) {
     setCartItem((state) => {
       const product = state.some((product) => product.productId === productId)
 
